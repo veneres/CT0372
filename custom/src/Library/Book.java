086ed7533ea4294dab48e87946957ee1bdd7d210
Veneri Alberto <veneri.alberto@gmail.com>
2019-10-04 17:04:14 +0200

    Changed NotImplemented Exception in RuntimeException

--- custom/src/Library/Book.java
+++ custom/src/Library/Book.java
@@ -1,7 +1,5 @@
 package Library;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
  * Deeply inspired by Introduction to programming in Java, MIT OpenCourseWare
  * https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-092-introduction-to-programming-in-java-january-iap-2010/
@@ -28,18 +26,18 @@ public class Book {
     }
 
     // Returns true if the book is rented, false otherwise
-    public boolean isBorrowed() {
+    public boolean isBorrowed(){
         // Implement this method
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented yet");
     }
 
     // Returns the title of the book
-    public String getTitle() {
+    public String getTitle(){
         // Implement this method
-        throw new NotImplementedException();
+        throw new RuntimeException("Not implemented yet");
     }
 
-    public static void main(String[] arguments) {
+    public static void main(String[] arguments){
         // Small test of the Book class
         Book example = new Book("The Da Vinci Code");
         System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
