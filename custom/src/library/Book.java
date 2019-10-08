819cd3bfad3b120a85cf71b668c6a78d1e232aa1
Alberto Veneri <veneri.alberto@gmail.com>
2019-10-08 10:28:09 +0200

    Package name changed and library solution added

---
+++ custom/src/library/Book.java
@@ -0,0 +1,51 @@
+package library;
+
+/**
+ * Deeply inspired by Introduction to programming in Java, MIT OpenCourseWare
+ * https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-092-introduction-to-programming-in-java-january-iap-2010/
+ * License: https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ */
+public class Book {
+
+    String title;
+    boolean borrowed;
+
+    // Creates a new Book
+    public Book(String bookTitle) {
+        // Implement this method
+    }
+
+    // Marks the book as rented
+    public void borrowed() {
+        // Implement this method
+    }
+
+    // Marks the book as not rented
+    public void returned() {
+        // Implement this method
+    }
+
+    // Returns true if the book is rented, false otherwise
+    public boolean isBorrowed(){
+        // Implement this method
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    // Returns the title of the book
+    public String getTitle(){
+        // Implement this method
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    public static void main(String[] arguments){
+        // Small test of the Book class
+        Book example = new Book("The Da Vinci Code");
+        System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
+        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+        example.borrowed();
+        System.out.println("Borrowed? (should be true): " + example.isBorrowed());
+        example.returned();
+        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+    }
+
+}
